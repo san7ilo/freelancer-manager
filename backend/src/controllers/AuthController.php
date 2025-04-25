83a785d5c00659bd3e717d87a2405d678ebd1bf0
JoseTorres3 <joseTM2404@gmail.com>
2025-04-24 22:15:22 -0500

    Change on AuthController

--- backend/src/controllers/AuthController.php
+++ backend/src/controllers/AuthController.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once '../vendor/autoload.php'; // Asegúrate de que el autoloader de Composer esté incluido
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
+require_once '../vendor/autoload.php'; 
 require_once '../helpers/Validator.php';
 require_once '../models/User.php';
 require_once '../services/AuthService.php';
@@ -91,6 +95,7 @@ class AuthController {
 
     private function response($statusCode, $message) {
         http_response_code($statusCode);
+        header('Content-Type: application/json'); 
         echo json_encode(['message' => $message]);
     }
 }
